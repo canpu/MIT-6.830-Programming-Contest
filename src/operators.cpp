4f219877766f20337b69573927a39053a786ed79
Can Pu <pucanmail@gmail.com>
2021-05-15 16:41:53 -0400

    Update: parallelized Join::run in row-first way

--- src/operators.cpp
+++ src/operators.cpp
@@ -1,7 +1,12 @@
 #include "operators.h"
 #include "omp.h"
+#include <set>
+#include <utility>
 
 #include <cassert>
+#include <iostream>
+
+using namespace::std;
 
 // Get materialized results
 std::vector<uint64_t *> Operator::getResults() {
@@ -128,7 +133,6 @@ void Join::run() {
     left_->run();
     right_->run();
 
-
     // Use smaller input_ for build
     if (left_->result_size() > right_->result_size()) {
         std::swap(left_, right_);
@@ -159,14 +163,39 @@ void Join::run() {
     for (uint64_t i = 0, limit = i + left_->result_size(); i != limit; ++i) {
         hash_table_.emplace(left_key_column[i], i);
     }
+
     // Probe phase
+    size_t left_data_size = copy_left_data_.size();
+    size_t right_data_size = copy_right_data_.size();
     auto right_key_column = right_input_data[right_col_id];
-    for (uint64_t i = 0, limit = i + right_->result_size(); i != limit; ++i) {
-        auto rightKey = right_key_column[i];
-        auto range = hash_table_.equal_range(rightKey);
+    uint64_t right_input_size = right_->result_size();
+    std::vector<std::pair<uint64_t, uint64_t>> pairs;
+    vector<uint64_t> t1, t2;
+    for (uint64_t right_id = 0; right_id < right_input_size; ++right_id) {
+        auto right_key_val = right_key_column[right_id];
+        auto range = hash_table_.equal_range(right_key_val);
         for (auto iter = range.first; iter != range.second; ++iter) {
-            copy2Result(iter->second, i);
+            uint64_t left_id = iter->second;
+            t1.push_back(left_id);
+            t2.push_back(right_id);
+            ++result_size_;
         }
+    }
+
+    // Materialization phase
+    for (unsigned cId = 0; cId < left_data_size + right_data_size; ++cId)
+        tmp_results_[cId].reserve(result_size_);
+
+    #pragma omp parallel for
+    for (size_t i = 0; i < result_size_; ++i) {
+        auto left_id = t1[i];
+        auto right_id = t2[i];
+
+        for (unsigned cId = 0; cId < left_data_size; ++cId)
+            tmp_results_[cId][i] = copy_left_data_[cId][left_id];
+
+        for (unsigned cId = 0; cId < right_data_size; ++cId)
+            tmp_results_[left_data_size+cId][i] = copy_right_data_[cId][right_id];
     }
 }
 
@@ -212,12 +241,42 @@ void SelfJoin::run() {
 
     auto left_col_id = input_->resolve(p_info_.left);
     auto right_col_id = input_->resolve(p_info_.right);
-
     auto left_col = input_data_[left_col_id];
     auto right_col = input_data_[right_col_id];
+
+//    std::unordered_map<uint64_t, std::set<unsigned>> left_hash;
+//    for (unsigned row_index = 0; row_index < input_->result_size(); ++row_index) {
+//        uint64_t val = left_col[row_index];
+//        if (left_hash.find(val) == left_hash.end()) {
+//            std::set<unsigned> tmp_set;
+//            tmp_set.emplace(row_index);
+//            left_hash.emplace(val, tmp_set);
+//        } else {
+//            left_hash[val].emplace(row_index);
+//        }
+//    }
+
+//    size_t num_cols = copy_data_.size();
+//    result_size_ = 0;
+//    for (unsigned row_index = 0; row_index < input_size; ++row_index) {
+//        uint64_t val = right_col[row_index];
+//        if (left_hash.find(val) != left_hash.end()) {
+//            for (unsigned left_index: left_hash[val]) {
+//                for (unsigned cId = 0; cId < num_cols; ++cId)
+//                    tmp_results_[cId].push_back(copy_data_[cId][row_index]);
+//                ++result_size_;
+//            }
+//        }
+//    }
+
+
     for (uint64_t i = 0; i < input_->result_size(); ++i) {
-        if (left_col[i] == right_col[i])
-            copy2Result(i);
+        if (left_col[i] == right_col[i]) {
+            size_t data_size = copy_data_.size();
+            for (unsigned cId = 0; cId < data_size; ++cId)
+                tmp_results_[cId].push_back(copy_data_[cId][i]);
+            ++result_size_;
+        }
     }
 }
 
